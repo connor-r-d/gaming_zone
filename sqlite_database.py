cd36daf0aa736a6b86c89e94e15116165cc116e3
connor-r-d <48093803+connor-r-d@users.noreply.github.com>
2019-12-03 16:16:16 +0000

    Add files via upload

---
+++ sqlite_database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('games.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE games (
+			id int,
+			game_title text
+			)""")
+			
+conn.commit()
+
+conn.close()
