4442771bbb84d136fbf6b8625585e386545147cc
connor-r-d <48093803+connor-r-d@users.noreply.github.com>
2019-12-04 18:06:55 +0000

    Delete script.js

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-$(document).ready(function(){
-	'use strict';
-		$('.read_more').click(function() {
-				$('.page_overlay').css('display', 'block');
-				$('.overlay').css('display', 'block');
-		});
-});
