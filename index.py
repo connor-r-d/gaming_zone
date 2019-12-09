a1b42d108e480d8fb419fe0bce91bfffb2c0bff6
Connor Duthie <cduthie@DESKTOP-JCBNH0Q.localdomain>
2019-12-09 17:54:37 +0000

    Advanced web tech

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ db_location = 'var/gaming_zone.db'
 def get_db():
     db = getattr(g, 'db', None)
     if db is None:
-        db = sqlite3.connect(db_location)
+        db = sql.connect(db_location)
         g.db = db
     return db
 
@@ -27,13 +27,39 @@ def init_db():
 
 @app.route('/')
 def home():
+    #db = get_db()
+    #db.cursor().execute('insert into games values (1, "The Last of Us", "ps4")')
+    #db.cursor().execute('insert into games values (2, "Mass effect", "three")')
+    #db.cursor().execute('insert into games values (3, "The legend of Zelda", "switch")')
+    #db.cursor().execute('insert into games values (4, "Halo Infinite", "xbox")')
+    #db.cursor().execute('insert into games values (5, "Witcher 3", "four")')
+    #db.cursor().execute('insert into games values (6, "Dark Souls", "four")')
+    #db.commit()
+
     con = sql.connect("var/gaming_zone.db")
     con.row_factory = sql.Row
 
     cur = con.cursor()
-    cur.execute("SELECT * FROM games")
-    rows = cur.fetchall();
-    return render_template('index.html', rows = rows)
+
+    cur.execute("SELECT * FROM games WHERE id=1")
+    the_last = cur.fetchall();
+    
+    cur.execute("SELECT * FROM games WHERE id=2")
+    mass_effect = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=3")
+    the_legend = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=4")
+    halo = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+
+    return render_template('index.html', the_last=the_last, mass_effect=mass_effect, the_legend=the_legend, halo=halo, witcher=witcher, dark_souls=dark_souls)
 
 @app.route('/xbox/games', methods=['GET', 'POST'])
 def xbox_games():
--- schema.sql
+++ schema.sql
@@ -1,6 +1,8 @@
 DROP TABLE if EXISTS games;
 
 CREATE TABLE games(
-	game_title varchar(100),
-	platform varchar(100)
+	id INTEGER NOT NULL,
+	game_title varchar(100) NOT NULL,
+	platform varchar(100) NOT NULL,
+	PRIMARY KEY (id)
 );
