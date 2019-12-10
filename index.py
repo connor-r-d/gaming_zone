18f783ffe4a57a8a65533305bb80f026f579a186
connor-r-d <48093803+connor-r-d@users.noreply.github.com>
2019-12-10 17:36:30 +0000

    Add files via upload

---
+++ index.py
@@ -0,0 +1,230 @@
+from flask import Flask, g, render_template, url_for, redirect
+import sqlite3 as sql
+
+from flask import Flask
+app = Flask(__name__)
+db_location = 'var/gaming_zone.db'
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = sql.connect(db_location)
+        g.db = db
+    return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.route('/')
+def home():
+    #db = get_db()
+    #db.cursor().execute('insert into games values (1, "The Last of Us", "ps4")')
+    #db.cursor().execute('insert into games values (2, "Mass effect", "three")')
+    #db.cursor().execute('insert into games values (3, "The legend of Zelda", "switch")')
+    #db.cursor().execute('insert into games values (4, "Halo Infinite", "xbox")')
+    #db.cursor().execute('insert into games values (5, "Witcher 3", "four")')
+    #db.cursor().execute('insert into games values (6, "Dark Souls", "four")')
+    #db.commit()
+
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=1")
+    the_last = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=2")
+    mass_effect = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=3")
+    the_legend = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=4")
+    halo = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+
+    return render_template('index.html', the_last=the_last, mass_effect=mass_effect, the_legend=the_legend, halo=halo, witcher=witcher, dark_souls=dark_souls)
+
+@app.route('/xbox', methods=['GET', 'POST'])
+def xbox_games():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=2")
+    mass_effect = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=4")
+    halo = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+    return render_template('xbox_games.html', mass_effect=mass_effect, halo=halo, witcher=witcher, dark_souls=dark_souls)
+
+@app.route('/playstation', methods=['GET', 'POST'])
+def playstation_games():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=1")
+    the_last = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=2")
+    mass_effect = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+
+    return render_template('playstation_games.html', the_last=the_last, mass_effect=mass_effect, witcher=witcher, dark_souls=dark_souls)
+
+@app.route('/nintendo', methods=['GET', 'POST'])
+def nintendo_games():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=3")
+    the_legend = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+    return render_template('nintendo_games.html', the_legend=the_legend, witcher=witcher, dark_souls=dark_souls)
+
+@app.route('/pc', methods=['GET', 'POST'])
+def pc_games():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=2")
+    mass_effect = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+    return render_template('pc.html', mass_effect=mass_effect, witcher=witcher, dark_souls=dark_souls)
+
+@app.route('/games/the_last_of_us', methods=['GET', 'POST'])
+def the_last_of_us():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=1")
+    the_last = cur.fetchall();
+	
+	if request.method == 'POST':
+		try:
+			titl = request.form['titl']
+			cont = request.form['cont']
+			desc = request.form['desc']
+			
+			with sql.connect("var/gaming_zone.db") as con:
+				cur = con.cursor()
+				cur.execute("INSERT INTO the_last_of_us (title,content,descriptors) VALUES (?,?,?)",(titl,cont,desc) )
+				
+				con.commit()
+			except:
+				con.rollback()
+			
+	cur.execute("SELECT * FROM the_last_of_us")
+	the_last_posts = cur.fetchall()
+
+    return render_template('the_last_of_us.html', the_last=the_last, the_last_posts=the_last_posts)
+	con.close()
+
+@app.route('/games/mass_effect', methods=['GET', 'POST'])
+def mass_effect():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=2")
+    mass_effect = cur.fetchall();
+
+    return render_template('mass_effect.html', mass_effect=mass_effect)
+
+@app.route('/games/the_legend_of_zelda', methods=['GET', 'POST'])
+def the_legend_of_zelda():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=3")
+    the_legend = cur.fetchall();
+
+    return render_template('the_legend.html', the_legend=the_legend)
+
+@app.route('/games/halo_infinite', methods=['GET', 'POST'])
+def halo_infinite():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=4")
+    halo = cur.fetchall();
+
+    return render_template('halo.html', halo=halo)
+
+@app.route('/games/witcher_3', methods=['GET', 'POST'])
+def witcher_three():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=5")
+    witcher = cur.fetchall();
+
+    return render_template('witcher.html', witcher=witcher)
+
+@app.route('/games/dark_souls', methods=['GET', 'POST'])
+def dark_souls():
+    con = sql.connect("var/gaming_zone.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM games WHERE id=6")
+    dark_souls = cur.fetchall();
+
+    return render_template('dark_souls.html', dark_souls=dark_souls)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
