c6bcda016c73e0c4955c2a8c08ec53007aef1e63
connor-r-d <48093803+connor-r-d@users.noreply.github.com>
2019-12-10 17:36:56 +0000

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE if EXISTS games;
+
+CREATE TABLE games(
+	id INTEGER NOT NULL,
+	game_title varchar(100) NOT NULL,
+	platform varchar(100) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+DROP TABLE if EXISTS the_last_of_us;
+
+CREATE TABLE the_last_of_us(
+	id INTEGER NOT NULL,
+	title varchar(100) NOT NULL,
+	content varchar(10000) NOT NULL,
+	descriptors varchar(150) NOT NULL,
+	PRIMARY KEY (id)
+);
